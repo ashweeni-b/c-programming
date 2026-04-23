fe14e35ee3da5585bfd427e44f06a1de2f6b3a77
ashweeni-b <bhardeashweeni05@gmail.com>
2026-04-24 03:09:30 +0530

    First program - Hello World

---
+++ 01_basics/hello_world.c
@@ -0,0 +1,13 @@
+/*
+Problem: Print "Hello World"
+Concept: First C Program
+Approach: Using library function - printf()
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!");
+
+    return 0;
+}
