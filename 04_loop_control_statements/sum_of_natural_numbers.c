549d4e8949964ae718a44db8a366465689ff94df
ashweeni-b <bhardeashweeni05@gmail.com>
2026-05-05 22:43:16 +0530

    refactor: Sum of natural numbers program

--- 04_loop_control_statements/sum_of_natural_numbers.c
+++ 04_loop_control_statements/sum_of_natural_numbers.c
@@ -25,33 +25,35 @@ Approach:
 #include <stdio.h>
 
 int main() {
-    int i, sum;
+    int i;
 
     // Calculate the sum using while loop
-    i = 1, sum = 0;
+    i = 1;
+    int sum_while_loop = 0;
     while(i <= 10) {
-        sum = sum + i;
+        sum_while_loop = sum_while_loop + i;
         i++;
     }
 
     // Calculate the sum using for loop
-    sum = 0;
+    int sum_for_loop = 0;
     for (int i = 1; i <= 10; i++) {
-        sum = sum + i;
+        sum_for_loop = sum_for_loop + i;
     }
 
     // Calculate the sum using do-while loop
-    i = 1; sum = 0;
+    i = 1; 
+    int sum_do_while_loop = 0;
     do {
-        sum = sum + i;
+        sum_do_while_loop = sum_do_while_loop + i;
         i++;
     } while (i <= 10);
 
     // Print the value of sum
     printf("Sum of first 10 natural numbers:\n");
-    printf("Using while loop is %d\n", sum);
-    printf("Using for loop is %d\n", sum);
-    printf("Using do-while loop is %d", sum);
+    printf("Using while loop is %d\n", sum_while_loop);
+    printf("Using for loop is %d\n", sum_for_loop);
+    printf("Using do-while loop is %d", sum_do_while_loop);
 
     return 0;
 }
