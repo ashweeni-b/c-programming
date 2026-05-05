b1c5636ee9e1cd21b46fa3c771703cf5b8d1b0f4
ashweeni-b <bhardeashweeni05@gmail.com>
2026-05-06 00:18:43 +0530

    feat: Update prime number check program with other loops
    Prime number check program with while loop and do-while loop

--- 04_loop_control_statements/prime_number_check.c
+++ 04_loop_control_statements/prime_number_check.c
@@ -5,35 +5,86 @@ Approach:
 Input number to be checked
 Check for divisors between 1 to number
 If divisors are there the number is not prime
-Else number is priem
+Else number is prime
 */
 
 #include <stdio.h>
 
 int main() {
-    int number, not_prime = 0;
+    int number;
 
     // Input number to be checked
     printf("Enter number: ");
     scanf("%d", &number);
 
-    // Check prime or not
+    // Check prime or not using for loop
+    int not_prime_for_loop = 0;
+
     for (int i = 2; i < number; i++) {
         if(number % i == 0) {
-            not_prime++;
+            not_prime_for_loop++;
             break;
         }
     }
     
     // Print output
+    printf("Using for loop: \n");
     if(number == 1) {
         printf("1 is neither prime nor composite");
     } else if(number == 2) {
         printf("2 is even prime number");
-    } else if(not_prime) {
-        printf("%d is not prime", number);
+    } else if(not_prime_for_loop) {
+        printf("%d is not prime number", number);
     } else {
-        printf("%d is prime", number);
+        printf("%d is prime number", number);
+    }
+
+    // Check prime or not using while loop
+    int not_prime_while_loop = 0;
+    int i = 2;
+
+    while (i < number) {
+        if(number % i == 0) {
+            not_prime_while_loop++;
+            break;
+        }
+        i++;
+    }
+    
+    // Print output
+    printf("\nUsing while loop: \n");
+    if(number == 1) {
+        printf("1 is neither prime nor composite");
+    } else if(number == 2) {
+        printf("2 is even prime number");
+    } else if(not_prime_while_loop) {
+        printf("%d is not prime number", number);
+    } else {
+        printf("%d is prime number", number);
+    }
+
+    // Check prime or not using do-while loop
+    int not_prime_do_while_loop = 0;
+    int j = 2;
+
+    do {
+        if(number % j == 0) {
+            not_prime_do_while_loop++;
+            break;
+        }
+        j++;
+    } while(j < number);
+    
+    // Print output
+    printf("\nUsing do-while loop: \n");
+    if(number == 1) {
+        printf("1 is neither prime nor composite");
+    } else if(number == 2) {
+        printf("2 is even prime number");
+    } else if(not_prime_do_while_loop) {
+        printf("%d is not prime number", number);
+    } else {
+        printf("%d is prime number", number);
     }
     
     return 0;
